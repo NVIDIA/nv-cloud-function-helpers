07b8f51b458fa19b4a7765dabb643d02b2dd1e14
Zhenwei Hu <zhenweih@nvidia.com>
2025-10-15 15:58:01 +0800

    [helpers] test helm chart

--- examples/fastapi_echo_sample/http_echo_server.py
+++ examples/fastapi_echo_sample/http_echo_server.py
@@ -4,6 +4,7 @@ import uvicorn
 from pydantic import BaseModel
 from fastapi import FastAPI, status
 from fastapi.responses import StreamingResponse
+import requests
 
 
 app = FastAPI()
@@ -16,15 +17,35 @@ def get_health() -> HealthCheck:
     return HealthCheck(status="OK")
 
 class Echo(BaseModel):
-    message: str
+    message: str = ""
     delay: float = 0.000001
     repeats: int = 1
     stream: bool = False
+    url: str = ""
 
 
 @app.post("/echo")
 async def echo(echo: Echo):
-    if echo.stream:
+    if echo.url:
+        try:
+            response = requests.get(echo.url)
+            s = f"""
+                code: {response.status_code}
+                text: {response.text}
+                
+            """
+            return s
+        except requests.exceptions.RequestException as e:
+            return str(e)
+        except requests.exceptions.HTTPError as e:
+            return str(e)
+        except requests.exceptions.ConnectionError as e:
+            return str(e)
+        except requests.exceptions.Timeout as e:
+            return str(e)
+        except requests.exceptions.RequestException as e:
+            return str(e)
+    elif echo.stream:
         def stream_text():
             for _ in range(echo.repeats):
                 time.sleep(echo.delay)
