f345b4d027fd4c782aa7d76a10f9dba8d1a1da6d
Zhenwei Hu <zhenweih@nvidia.com>
2025-10-29 10:46:13 +0800

    x

--- examples/fastapi_echo_sample/http_echo_server.py
+++ examples/fastapi_echo_sample/http_echo_server.py
@@ -5,6 +5,11 @@ from pydantic import BaseModel
 from fastapi import FastAPI, status
 from fastapi.responses import StreamingResponse
 import requests
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+from opentelemetry.metrics import get_meter_provider, set_meter_provider
 
 
 app = FastAPI()
@@ -26,6 +31,9 @@ class Echo(BaseModel):
 
 @app.post("/echo")
 async def echo(echo: Echo):
+    echo_requests_total.add(1)
+    active_connections.inc()
+    
     if echo.url:
         try:
             response = requests.get(echo.url)
@@ -52,8 +60,29 @@ async def echo(echo: Echo):
                 yield f"data: {echo.message}\n\n"
         return StreamingResponse(stream_text(), media_type="text/event-stream")
     else:
+        start = time.time()
         time.sleep(echo.delay)
-        return echo.message*echo.repeats
+        result = echo.message*echo.repeats
+        response_time.observe(time.time() - start)
+        active_connections.dec()
+        return result
+
+
+def init_observability():
+    global echo_requests_total
+    resource = Resource(attributes={"service.name": os.getenv("OTEL_SERVICE_NAME", "http-echo")})
+    metric_exporter = OTLPMetricExporter()
+    reader = PeriodicExportingMetricReader(
+        metric_exporter, export_interval_millis=60_000
+    )
+    meter_provider = MeterProvider(metric_readers=[reader], resource=resource)
+    set_meter_provider(meter_provider)
+    meter = get_meter_provider().get_meter("my_meeter")
+    echo_requests_total = meter.create_counter(
+        "echo_requests_total", "counter", "Total number of echo requests"
+    )
+
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, workers=int(os.getenv('WORKER_COUNT', 500)))
+    init_observability()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
